2693c26f043cad325f27b42356d17c482a58cec6
Blackvenda <155356360+Blackvenda@users.noreply.github.com>
2024-01-04 19:47:17 +0530

    Update qoutityManager.js

--- qoutityManager.js
+++ qoutityManager.js
@@ -1,10 +1,1 @@
-const checkAvilableqtt = document.querySelectorAll(".contant-detaiels-here")[0].innerHTML.split("Quantity-[")[1].split("]")[0];
-const availablestk = document.querySelectorAll(".contant-detaiels-here")[0].innerHTML.split("Stock Lavel-[")[1].split("]")[0];
-const changeQtty = document.querySelectorAll("#quantity")[0];
-
-document.querySelectorAll(".qtyAviBalDis")[0].innerHTML =availablestk;
-document.querySelectorAll(".testing")[0].innerHTML =checkAvilableqtt;
-
-if(changeQtty.match("checkAvilableqtt")){
 	document.querySelector(".plus-btn").setAttribute("disabled", "disabled");
-}
