f2df89c544f6d05658cf1e0249e595722f761e5e
Blackvenda <155356360+Blackvenda@users.noreply.github.com>
2024-01-09 12:00:13 +0530

    Delete accountSetLoading.js

--- accountSetLoading.js
+++
@@ -1,6 +0,0 @@
-    window.onload = function () {
-        var getEmailAddressHereforAccountsetting = document.getElementById("customer-nail-dd").innerText;
-        var getUsernameforAccountsettings = document.getElementById("customer-name").innerText;
-        document.getElementById("userEm").innerText = getEmailAddressHereforAccountsetting;
-        document.getElementById("userNm").innerText = getUsernameforAccountsettings.toLowerCase();
-    };
