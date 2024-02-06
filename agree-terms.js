53c91dcbe2c0d00b280d3c46a6f2e7b4e0ea067d
Blackvenda <155356360+Blackvenda@users.noreply.github.com>
2024-02-06 19:38:18 +0530

    Create agree-terms.js

---
+++ agree-terms.js
@@ -0,0 +1,13 @@
+function submitOrder() {
+    // This function can be modified to handle form submission
+    alert("Order placed successfully!");
+}
+
+document.getElementById("termsCheckbox").addEventListener("change", function() {
+    var submitButton = document.getElementById("paymentOp");
+    if (this.checked) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
