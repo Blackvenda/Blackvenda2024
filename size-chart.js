cea23d843bb8fa909cb711aa593d8bd9d23b0c8b
Blackvenda <155356360+Blackvenda@users.noreply.github.com>
2024-02-07 20:32:30 +0530

    Create size-chart.js

---
+++ size-chart.js
@@ -0,0 +1,9 @@
+// Function to display the custom popup
+function openCustomPopup() {
+    document.getElementById('customPopupContainer').style.display = 'block';
+}
+
+// Function to close the custom popup
+function closeCustomPopup() {
+    document.getElementById('customPopupContainer').style.display = 'none';
+}
