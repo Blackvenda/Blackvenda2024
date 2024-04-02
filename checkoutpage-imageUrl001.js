82756d93d938e46f311567a01e1ac77ace15808a
Blackvenda <155356360+Blackvenda@users.noreply.github.com>
2024-04-02 22:24:45 +0530

    Update checkoutpage-imageUrl002.js

--- checkoutpage-imageUrl001.js
+++ checkoutpage-imageUrl001.js
@@ -2,11 +2,10 @@
     function setThumbnailUrlToInputField() {
         var thumbnailUrl = getImageUrl(); // Get thumbnail image URL
         document.getElementById("productImgUrl").value = thumbnailUrl; // Set input field value
+        window.onload=function(){document.getElementById('CurrentProductURL').value=window.location.href
     }
 
     // Call the function after the document has fully loaded
     window.addEventListener('DOMContentLoaded', function() {
         setThumbnailUrlToInputField();
     });
-
-window.onload=function(){document.getElementById('CurrentProductURL').value=window.location.href
